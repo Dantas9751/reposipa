ca26350e7c46d067a0d74edca2c4f1e4851b3c07
nizaki9751 <tpodol@gmail.com>
2026-06-10 20:14:46 -0300

    validacao certa

--- validacao.js
+++ validacao.js
@@ -1,32 +1,27 @@
 const form = document.getElementById("formContato");
 
 form.addEventListener("submit", function(event) {
-
-    const nome = document.getElementById("nome").value;
-    const email = document.getElementById("email").value;
-    const mensagem = document.getElementById("mensagem").value;
-
-    const regexNome = /^[A-Za-zÀ-ÿ\s]+$/;
-    const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-    if (!regexNome.test(nome)) {
-        alert("Favor inserir nome completo.");
+    if (!form.checkValidity()) {
         event.preventDefault();
-        return;
     }
+});
 
-    if (!regexEmail.test(email)) {
-        alert("E-mail inválido.");
-        event.preventDefault();
-        return;
-    }
+const nomeInput = document.getElementById("nome");
+nomeInput.addEventListener("input", () => {
+    nomeInput.value = nomeInput.value.replace(/\d/g, "");
+});
 
-    if (mensagem.length < 20) {
-        alert("A mensagem deve ter pelo menos 20 caracteres.");
-        event.preventDefault();
-        return;
-    }
+const emailInput = document.getElementById("email");
+emailInput.addEventListener("input", () => {
+    console.log(emailInput.value);
+});
 
-    alert("Formulário enviado.");
+const assuntoInput = document.getElementById("assunto");
+assuntoInput.addEventListener("input", () => {
+    console.log(assuntoInput.value);
+});
 
+const mensagemInput = document.getElementById("mensagem");
+mensagemInput.addEventListener("input", () => {
+    console.log(mensagemInput.value);
 });
